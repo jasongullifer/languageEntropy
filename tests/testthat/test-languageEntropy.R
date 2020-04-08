eba8d1f0f27a7b6fd0f1f06bbd9b3cf1a6fe665d
Jason Gullifer <jason.gullifer@gmail.com>
2020-04-08 18:37:43 -0400

    added tests for languageEntropy function to check that results are equivalent w/ and w/o use of colsList argument.

--- tests/testthat/test-languageEntropy.R
+++ tests/testthat/test-languageEntropy.R
@@ -16,11 +16,12 @@ percent_interval = 5
 
 ## compute entropy
 cols <- colnames(likerts %>% select(contains("_prop")))
+percents.colslist <- languageEntropy(percents, id = sub, base=base, colsList = list(percent=c("L1_prop","L2_prop")))
 likerts <- languageEntropy(likerts, id = sub, !!cols, contextName = "prop", base = base)
 percents <- languageEntropy(percents, id = sub, L1_prop, L2_prop, contextName = "percent", base=base)
+
 # should run an entropy computation by hand and cross check with the function
 #### HERE
-
 # ## TEST for equality between by hand entropy and languageEntropy()
 # likerts$entropy_test <- likerts$prop.entropy == likerts$p.entropy
 #
@@ -40,11 +41,21 @@ test_that("min entropy is 0", {
   expect_equal(min(likerts$prop.entropy, na.rm=T), 0)
 })
 
-
 duppercents <- rbind(percents, percents[percents$sub=="15",])
-
 # TEST that languageEntropy throws error with duplicate subjects
 test_that("languageEntropy throws error with duplicate subjects",{
   expect_error(languageEntropy(duppercents, id = sub, L1_prop, L2_prop, contextName = "percent", base=base),
                "You have duplicate identifiers in your data: 15")
+})
+
+# See if we catch the warning when not specifying context name
+test_that("languageEntropy throws a warning when user doesn't specify contextName",{
+  expect_warning(languageEntropy(percents, id = sub, L1_prop, L2_prop, base=base),
+                 "You didn't name your usage context by supplying a 'contextName' argument. I'm going to name your context 'mycontext.' Note if this happens again, your entropy value will be overwritten, so it is better to give an actual name."
+                 )
+})
+
+# Compare results of languageEntropy w/ colsList to w/o colsList
+test_that("check that results are the same w/ and w/o using colsList argument",{
+  expect_equal(percents, percents.colslist)
 })
